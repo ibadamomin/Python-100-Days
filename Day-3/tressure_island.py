47a027faf4d1f95cd2a3a4a02a71000a567a2a7e
Ibad Momin <46007141+ibadamomin@users.noreply.github.com>
2021-08-15 09:21:02 -0500

    Create tressure_island.py

---
+++ Day-3/tressure_island.py
@@ -0,0 +1,41 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+#print('''Designed by Aizza Momin
+#and coded by Ibad Momin''')
+
+print("Welcome to Outer Banks.")
+print("Your mission is to find the Sponge.") 
+
+first_choice = input("You are across the sea, where do you want to go? Choose Left or Right:").lower()
+
+if (first_choice == "right"):
+  next_choice = input("Infront of you there is a hole, choose to either swim or jump:").lower()
+  if (next_choice == "jump"):
+    third_choice = input("As you go further down, there are three doors, choose Blue, Yellow, or Red:").lower()
+    if(third_choice == "blue"):
+      fourth_choice = input("There is a long way to go, choose skip rope or run:").lower()
+      if (fourth_choice == "skip rope"):
+       print ("You won!")        
+else: 
+  print ("Game Over. You Loser!")
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
